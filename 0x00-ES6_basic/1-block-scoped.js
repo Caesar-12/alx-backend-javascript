15a9b748335aab4b3075c26469c008a5a6c9d9e4
Caesar-12 <idowuenoch65@gmail.com>
2023-11-14 00:59:57 +0000

    Declarations

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,11 +3,8 @@ export default function taskBlock(trueOrFalse) {
   let task2 = true;
 
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    task = false;
+    task2 = true;
   }
   return [task, task2];
 }
-
-console.log(taskBlock(true));
-console.log(taskBlock(false));

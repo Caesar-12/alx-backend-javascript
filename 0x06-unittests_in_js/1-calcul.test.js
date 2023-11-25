928ca3da379038554c1efbab46600f6a439e787f
Caesar-12 <idowuenoch65@gmail.com>
2023-11-25 01:14:27 +0000

    Assert test function

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -4,4 +4,4 @@ const assert = require('assert');
 assert.strictEqual(calculateNumber("SUM", 1.2, 2.3), 3);
 assert.strictEqual(calculateNumber("SUBTRACT", 5, 2.7), 2);
 assert.strictEqual(calculateNumber("DIVIDE", 7.5, 2), 4);
-assert.strictEqual(calculateNumber("DIVIDE", 5.8, 0), Error);
+assert.strictEqual(calculateNumber("DIVIDE", 5.8, 0), 'Error');

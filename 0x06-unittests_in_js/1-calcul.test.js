2097089688468389a5660a2aac4959c20f3dea8d
Caesar-12 <idowuenoch65@gmail.com>
2023-11-25 01:01:22 +0000

    Assert

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,7 @@
+const calculateNumber = require("./1-calcul.js");
+const assert = require('assert');
+
+assert.strictEqual(calculateNumber("SUM", 1.2, 2.3), 3);
+assert.strictEqual(calculateNumber("SUBTRACT", 5, 2.7), 2);
+assert.strictEqual(calculateNumber("DIVIDE", 7.5, 2), 4);
+assert.strictEqual(calculateNumber("DIVIDE", 5.8, 0), Error);

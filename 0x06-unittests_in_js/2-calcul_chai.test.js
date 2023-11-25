87ea8e0ffe95317ea0121bb733a75266ffbe2367
Caesar-12 <idowuenoch65@gmail.com>
2023-11-25 01:35:47 +0000

    Expect chai assertion function

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,7 @@
+const calculateNumber = require("./1-calcul.js");
+const { expect } = require('chai');
+
+expect(calculateNumber('SUM', 1.2, 3.4)).to.equal(4);
+expect(calculateNumber('SUBTRACT', 5.5, 3.4)).to.equal(3);
+expect(calculateNumber('DIVIDE', 7.7, 3.6)).to.equal(2);
+expect(calculateNumber('DIVIDE', 14.5, 0)).to.equal('Error');

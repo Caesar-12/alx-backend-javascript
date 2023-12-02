5e90356c7dfa339bbf01f7d0718d40e20fb0a599
Caesar-12 <idowuenoch65@gmail.com>
2023-12-02 01:24:34 +0000

    Resolved the promis

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  uploadPhoto()
-    .then((result) => { process.stdout.write(`${result.body} `); })
-    .catch(() => { console.log('Signup system offline'); });
-
-  createUser()
-    .then((result) => { console.log(`${result.firstName} ${result.lastName}`); })
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([up, cu]) => {
+      console.log(`${up.body} ${cu.firstName} ${cu.lastName}`);
+    })
     .catch(() => { console.log('Signup system offline'); });
 }

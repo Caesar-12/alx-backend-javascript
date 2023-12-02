4e2ad82a179f84d3b939bcfc8f46666f675a046d
Caesar-12 <idowuenoch65@gmail.com>
2023-12-02 01:37:05 +0000

    Resolving promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,6 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup() {
+export function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
     .then(([up, cu]) => {
       console.log(`${up.body} ${cu.firstName} ${cu.lastName}`);

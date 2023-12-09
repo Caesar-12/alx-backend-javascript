7ea05f86d1bc6bc943e3903d16c8d58ed000eec0
Caesar-12 <idowuenoch65@gmail.com>
2023-12-09 17:49:17 +0000

    Methods

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,36 @@
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    if (typeof newAmount === 'number') {
+      this._amount = newAmount;
+    } else {
+      console.error('TypeError: Amount must be a number');
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    if (typeof newCurrency === 'object') {
+      this._currency = newCurrency;
+    }
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency._name} (${this._currency._code})`;
+  }
+
+  convertPrice(amount, conversionRate) {
+    return (amount * conversionRate);
+  }
+}

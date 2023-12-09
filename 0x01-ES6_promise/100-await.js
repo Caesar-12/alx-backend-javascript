635141f4509fd331800d4ea84183ab42542fa5c9
Caesar-12 <idowuenoch65@gmail.com>
2023-12-09 15:06:42 +0000

    Promise and object

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -2,22 +2,26 @@ import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
   const retObj = {};
+  try {
+    await uploadPhoto()
+      .then((result) => {
+        retObj.photo = result;
+      })
+      .catch(() => {
+        retObj.photo = null;
+      });
 
-  await uploadPhoto()
-    .then((result) => {
-      retObj.photo = result;
-    })
-    .catch(() => {
-      retObj.photo = null;
-    });
-
-  await createUser()
-    .then((result) => {
-      retObj.user = result;
-    })
-    .catch(() => {
-      retObj.user = null;
-    });
+    await createUser()
+      .then((result) => {
+        retObj.user = result;
+      })
+      .catch(() => {
+        retObj.user = null;
+      });
+  } catch (error) {
+    retObj.photo = null;
+    retObj.user = null;
+  }
 
   return retObj;
 }

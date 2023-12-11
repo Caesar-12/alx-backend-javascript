81bb04b4a5f726dc87ae6963d1b1b40b77d7ed87
Caesar-12 <idowuenoch65@gmail.com>
2023-12-11 01:38:34 +0000

    Abstract classes

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,20 +1,13 @@
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
-    if (typeof this.evacuationWarningMessage !== 'function') {
+
+    if (typeof this.evacuationWarningMessage !== 'function' && this.constructor.name !== 'Building') {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  set sqft(newSqft) {
-    if (typeof newSqft === 'number') {
-      this._sqft = newSqft;
-    } else {
-      console.error('TypeError: Sqft must be a number');
-    }
   }
 }
